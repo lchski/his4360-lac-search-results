743a6a3e6e01b629ed9f78b194b1faecfc75d2e4
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-03-04 22:46:30 -0500

    remove debug

--- load.R
+++ load.R
@@ -29,26 +29,5 @@ tbs_incumbent_data <- read_results_json("data/source/TBS - Incumbent System data
 
 na_electronic_fonds <- read_results_json("data/source/na - electronic fonds.json")
 
-results_raw <- read_json("data/source/na - electronic fonds.json", flatten = TRUE)
-
-## debugging na_electronic_fonds
-### somewhere in here (rows 501:600) we get an extra level of nesting
-results_raw$SearchResults$Records %>%
-  as_tibble() %>%
-  mutate(id = row_number()) %>%
-  select(id, value = Record) %>%
-  unnest(c(value)) %>%
-  unnest(c(value)) %>%
-  group_by(id) %>%
-  mutate(property_id = row_number(), name = map(value, names)) %>%
-  unnest(c(name, value)) %>%
-  unnest(c(value)) %>%
-  select(record_id = id, property_id, key = name, value) %>%
-  group_by(record_id, property_id) %>%
-  pivot_wider(names_from = key, values_from = value) %>%
-  ungroup() %>%
-  group_by(record_id, N, L) %>% summarise_all(paste, collapse = '\n\n---\n\n') %>% ## when we have multiple values for a property, combine into one string (otherwise pivot_longer snaps)
-  ungroup() %>%
-  pivot_wider(id_cols = record_id, names_from = N, values_from = V)
 
 
